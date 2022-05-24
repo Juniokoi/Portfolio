e165263936dd20d5cf5ab9a2c1661d92be7c8f1d
juniokoi <juniokoi@hotmail.com>
2022-05-24 19:34:42 -0300

    Add base website

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css'
+
 
 function App() {
 
   return (
-    <div>
+    <div className='bg-zinc-800 text-gray-200 h-[100vh] w-[100vw] flex flex-col items-center justify-center'>
+      <h1 className='mx-auto  font-bold text-xl'>Site em desenvolvimento! </h1>
+      <p>Um website incrível está por vir! </p>
     </div>
   )
 }
