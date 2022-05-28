df0c70df28daa15e9e3def3d8a8dc356aa14239d
juniokoi <juniokoi@hotmail.com>
2022-05-27 21:13:34 -0300

    Add Styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-
+import React from "react"
 
 function App() {
 
   return (
-    <div className='bg-zinc-800 text-gray-200 h-[100vh] w-[100vw] flex flex-col items-center justify-center'>
-      <h1 className='mx-auto  font-bold text-xl'>Site em desenvolvimento! </h1>
+    <div>
+      <h1>Site em desenvolvimento! </h1>
       <p>Um website incrível está por vir! </p>
     </div>
   )
