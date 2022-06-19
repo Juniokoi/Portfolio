633980e271d00c81901249f3191a1d907829e058
Junio Koi <juniokoi@hotmail.com>
2022-06-19 18:06:01 -0300

    Split file to be easier to config

---
+++ src/components/About/Skills/SkillTypes.ts
@@ -0,0 +1,72 @@
+const SkillTypes = [
+  {
+    title: "Front-end",
+    description: "Testeeee",
+    skills: [
+      {
+        title: ".JS",
+        img: "javascript",
+        alias: "ícone do JavaScript",
+      },
+      {
+        title: "React",
+        img: "react",
+        alias: "ícone do React",
+      },
+      {
+        title: ".HTML",
+        img: "html",
+        alias: "ícone do HTML",
+      },
+      {
+        title: ".CSS",
+        img: "css",
+        alias: "ícone do CSS",
+      },
+    ],
+  },
+  {
+    title: "Back-end",
+    description: "Testeeee",
+    skills: [
+      {
+        title: "MongoDB",
+        img: "mongodb",
+        alias: "ícone do MongoDB",
+      },
+      {
+        title: "SQLite",
+        img: "sqlite",
+        alias: "ícone do PostgreSQL",
+      },
+      {
+        title: "Node JS",
+        img: "node",
+        alias: "ícone do PostgreSQL",
+      },
+      {
+        title: "Databases",
+        img: "database",
+        alias: "ícone de um database",
+      },
+    ],
+  },
+  {
+    title: "Design UI",
+    description: "Testeeee",
+    skills: [
+      {
+        title: "Figma",
+        img: "",
+        alias: "ícone do MongoDB",
+      },
+      {
+        title: "sla",
+        img: "",
+        alias: "ícone do PostgreSQL",
+      },
+    ],
+  },
+];
+
+export default SkillTypes;
